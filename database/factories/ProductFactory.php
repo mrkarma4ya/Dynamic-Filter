472a00cc20ed655a57ef6d17cbb747583f65282c
Sarik Karmacharya <sarik.karmacharya@gmail.com>
2021-02-02 15:18:23 +0545

    changed random image api to unsplash

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -12,6 +12,7 @@ $factory->define(Product::class, function (Faker $faker) {
         'category_id' => \App\Category::inRandomOrder()->first()->id,
         'description' => $faker->paragraph(),
         'price' => rand(1000, 5000),
-        'main_image' => $faker->imageUrl($width = 700, $height = 400)
+        // 'main_image' => $faker->imageUrl($width = 700, $height = 400)
+        'main_image' => 'https://source.unsplash.com/random/700x400?sig='.rand(1,10000)
     ];
 });
